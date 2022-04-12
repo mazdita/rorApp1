4e368dbde07344264c80e4a9d174788e33442037
mazdita <mazdita@hotmail.com>
2022-04-12 11:58:46 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,25 @@
-require "test_helper"
 
+require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Ruby on Rails JobandTalent"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-  end
-
-  test "should get ror" do
+    assert_select "title", "Home | #{@base_title}"
+    end
+    test "should get ror" do
     get static_pages_ror_url
     assert_response :success
-  end
-
-  test "should get tech" do
-    get static_pages_tech_url
-    assert_response :success
-  end
+    assert_select "title", "Ror | #{@base_title}"
+    end
+    test "should get tech" do
+      get static_pages_tech_url
+      assert_response :success
+      assert_select "title", "Tech | #{@base_title}"
+    end
+      
 end
